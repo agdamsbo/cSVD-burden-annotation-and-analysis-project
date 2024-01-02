107825b444970b6c6737c162102010d35db05d5f
Andreas Gammelgaard Damsbo <andreas@gdamsbo.dk>
2024-01-02 11:18:14 +0100

    custom functions

--- R/functions.R
+++ R/functions.R
@@ -1,15 +1,18 @@
 #' Imports specified TALOS data
 #'
 #' @param token api token
+#' @param vars variable names to retrieve (fields in REDCap lingo)
 #'
 #' @return tibble of REDCap exported data
 #' @examples
 #' data <- import_talos()
 #' skimr::skim(data)
-import_talos <- function(key = "TALOS_REDCAP_API") {
+#' ds <- import_talos()
+import_talos <- function(key = "TALOS_REDCAP_API",
+                         vars = c("record_id", "inkl_rnumb", "cpr", "talos_inkl03x", "talos_basis02a", "talos_basis02b", "basis_sys_site")) {
   REDCapR::redcap_read(
     redcap_uri = "https://redcap.au.dk/api/", token = keyring::key_get(key),
-    fields = c("record_id", "inkl_rnumb", "cpr", "talos_inkl03x", "talos_basis02a", "talos_basis02b", "basis_sys_site")
+    fields = vars
   )$data
 }
 
@@ -31,13 +34,24 @@ filter_talos_site <- function(data, site = "10015") {
   }
 }
 
-next_id <- function(key = "SVD_REDCAP_API"){
-suppressMessages(
-    REDCapR::redcap_next_free_record_name(redcap_uri = "https://redcap.au.dk/api/",
-                                          token = keyring::key_get(key))
-    ) |>
-        as.numeric()
-    }
+#' Quick number of subjects in project
+#'
+#' @param key API key name for ekyring key retrieval
+#'
+#' @return numeric vector
+#' @export
+#'
+#' @examples
+#' redcap_get_n()
+redcap_get_n <- function(key = "SVD_REDCAP_API") {
+  REDCapR::redcap_read_oneshot(
+    redcap_uri = "https://redcap.au.dk/api/",
+    token = keyring::key_get(key),
+    fields = "record_id"
+  ) |>
+    purrr::pluck("data") |>
+    nrow()
+}
 
 #' Modifies exported talos data to new database format
 #'
@@ -46,24 +60,34 @@ suppressMessages(
 #' @param key destination project API key name
 #'
 #' @return tibble with modified data
-#' data |> modify_data()
-modify_data <- function(data, trial = "TALOS",key="SVD_REDCAP_API") {
-    ids <- seq_len(nrow(data))+(next_id(key=key)-1)
+#' ds |> modify_data(index=0)
+modify_data <- function(data,
+                        index = redcap_get_n(),
+                        trial = "TALOS",
+                        key = "SVD_REDCAP_API",
+                        id.var = inkl_rnumb,
+                        cpr.var = cpr,
+                        name.var = talos_inkl03x,
+                        date.var = talos_basis02a,
+                        time.var = talos_basis02b) {
+  ids <- seq_len(nrow(data)) + (index)
 
-    data |> dplyr::mutate(ids=ids) |>
-      dplyr::transmute(
-    record_id = glue::glue("svd_{ids}"),
-    trial_id = inkl_rnumb,
-    cpr = cpr,
-    name = talos_inkl03x,
-    index_date = lubridate::ymd(talos_basis02a),
-    index_time = hms::as_hms(talos_basis02b),
-    trial_name = dplyr::case_match(
-      trial,
-      "TALOS" ~ 1,
-      "RESIST" ~ 2
+  data |>
+    dplyr::mutate(ids = ids) |>
+    dplyr::transmute(
+      record_id = glue::glue("svd_{ids}"),
+      trial_id = {{ id.var }},
+      cpr = {{ cpr.var }},
+      name = {{ name.var }},
+      index_date = lubridate::ymd({{ date.var }}),
+      index_time = format(as.POSIXct({{ time.var }}), "%H:%M"),
+      trial_name = dplyr::case_match(
+        trial,
+        "TALOS" ~ 1,
+        "RESIST" ~ 2
+      ),
+      basis_complete = 2
     )
-  )
 }
 
 
@@ -73,6 +97,11 @@ modify_data <- function(data, trial = "TALOS",key="SVD_REDCAP_API") {
 #' @param data data to write
 #'
 #' @return
+#' @examples
+#' ds |>
+#'   filter_talos_site() |>
+#'   modify_data() |>
+#'   write2new_db()
 write2new_db <- function(data, key = "SVD_REDCAP_API") {
   REDCapR::redcap_write(
     redcap_uri = "https://redcap.au.dk/api/",
@@ -80,6 +109,3 @@ write2new_db <- function(data, key = "SVD_REDCAP_API") {
     ds_to_write = data
   )
 }
-
-
-# import_talos() |> filter_talos_site() |> modify_data() |> write2new_db()
