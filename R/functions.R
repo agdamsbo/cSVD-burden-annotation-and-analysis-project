2c0294aeef41c9f48bff9ed9c6c30e37d21bdec6
Andreas Gammelgaard Damsbo <andreas@gdamsbo.dk>
2024-01-09 11:22:30 +0100

    inter-rater calculations are ready. improvements to REDCapCAST as a consequence

--- R/functions.R
+++ R/functions.R
@@ -109,3 +109,116 @@ write2new_db <- function(data, key = "SVD_REDCAP_API") {
     ds_to_write = data
   )
 }
+
+
+## Inter-rater-reliability
+
+# https://stackoverflow.com/questions/71587719/inter-rater-reliability-by-groups
+# http://www.cookbook-r.com/Statistical_analysis/Inter-rater_reliability/
+#
+#   https://joon-e.github.io/tidycomm/reference/test_icr.html
+
+
+#' Sample data for inter-rater-reliability testing
+#'
+#' @return data.frame
+#' @export
+#' @examples
+#' irr_sample()
+irr_sample <- function() {
+  set.seed(8)
+  paste0("svd_", sample(1:5, 5, replace = FALSE)) |>
+    lapply(\(x){
+      rbind(
+        c(x, "A", sample(1:0, 20, replace = TRUE)),
+        c(x, "B", sample(1:0, 20, replace = TRUE))
+      ) |>
+        as.data.frame() |>
+        setNames(c("record_id", "svd_user", paste0("item", 1:20)))
+    }) |>
+    purrr::list_rbind()
+}
+
+#' Cleans ID numbers and arranges by increasing value
+#'
+#' @param data data set
+#' @param remove string to remove
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' irr_sample() |> clean_record_id()
+clean_record_id <- function(data, remove = "svd_") {
+  data |>
+    dplyr::mutate(record_id = stringr::str_remove(record_id, remove)) |>
+    dplyr::arrange(record_id)
+}
+
+remotes::install_github("agdamsbo/REDCapCAST")
+library(REDCapCAST)
+
+#' Read single REDCap instrument
+#'
+#' @param key API key
+#' @param instrument instrument name
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+#' read_instrument(key = "SVD_REDCAP_API", instrument = "svd_score")
+read_instrument <- function(key = "SVD_REDCAP_API", instrument = "svd_score", raw_label = "raw") {
+  REDCapCAST::read_redcap_tables(
+    uri = "https://redcap.au.dk/api/", token = keyring::key_get(key),
+    fields = "record_id",
+    forms = instrument, raw_or_label = raw_label
+  )[[instrument]]
+}
+
+
+#' Title
+#'
+#' @param key
+#' @param instrument
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' read_instrument() |> inter_rater_data()
+inter_rater_data <- function(data) {
+  data |>
+    clean_record_id() |>
+    dplyr::filter(svd_perf == 1) |>
+    dplyr::select(
+      record_id,
+      svd_user,
+      svd_quality,
+      svd_microbleed,
+      svd_microbleed_location___1,
+      svd_microbleed_location___2,
+      svd_microbleed_location___3,
+      svd_siderose,
+      svd_lacunes,
+      svd_wmh,
+      svd_atrophy
+    )
+}
+
+
+#' Inter rater reliability calculations
+#'
+#' @param data
+#'
+#' @return
+#' @export tibble
+#'
+#' @examples
+#' irr_sample |> inter_rater()
+#' read_instrument() |>
+#'   inter_rater_data() |>
+#'   inter_rater()
+inter_rater <- function(data) {
+  data |> tidycomm::test_icr(unit_var = record_id, coder_var = svd_user, fleiss_kappa = TRUE)
+}
